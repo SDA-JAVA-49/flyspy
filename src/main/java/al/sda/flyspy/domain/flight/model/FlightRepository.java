2418ad242d5be976b848758125382a1134350032
armirigj <armirgjini76@gmail.com>
2025-05-03 14:49:58 +0200

    Implemented methods

--- src/main/java/al/sda/flyspy/domain/flight/model/FlightRepository.java
+++ src/main/java/al/sda/flyspy/domain/flight/model/FlightRepository.java
@@ -1,4 +1,29 @@
 package al.sda.flyspy.domain.flight.model;
 
-public class FlightRepository {
+import al.sda.flyspy.domain.flight.model.entity.Flight;
+import al.sda.flyspy.shared.Repository;
+
+import java.util.Optional;
+import java.util.Set;
+
+public class FlightRepository implements Repository<Flight, String>{
+    @Override
+    public Optional<Flight> findById(String identifier) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Flight save(Flight object) {
+        return null;
+    }
+
+    @Override
+    public Set<Flight> getAll() {
+        return Set.of();
+    }
+
+    @Override
+    public void delete(String s) {
+
+    }
 }
