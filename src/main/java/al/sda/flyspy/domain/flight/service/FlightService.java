6e502df381e2781f273b855ce1125c2c2eb20f65
silvio.progonati <silvio.progonati@lhind.dlh.de>
2025-05-18 11:34:27 +0200

    Add unit test for Flight Service

--- src/main/java/al/sda/flyspy/domain/flight/service/FlightService.java
+++ src/main/java/al/sda/flyspy/domain/flight/service/FlightService.java
@@ -2,6 +2,10 @@ package al.sda.flyspy.domain.flight.service;
 
 import al.sda.flyspy.domain.flight.model.dto.FlightDto;
 import al.sda.flyspy.domain.flight.model.dto.airdata.AirData;
+import al.sda.flyspy.domain.flight.model.dto.airdata.Aircraft;
+import al.sda.flyspy.domain.flight.model.dto.airdata.Airline;
+import al.sda.flyspy.domain.flight.model.dto.airdata.FlightIdentifier;
+import al.sda.flyspy.domain.flight.model.dto.airdata.TerminalPoint;
 import al.sda.flyspy.domain.flight.model.entity.Flight;
 import al.sda.flyspy.shared.Repository;
 
@@ -9,10 +13,12 @@ import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public class FlightService {
     private final Repository<Flight, Long> repository;
     private final AirDataService airDataService;
+    private static final String DEFAULT_VALUE = "-";
 
     public FlightService(Repository<Flight, Long> repository, AirDataService airDataService) {
         this.repository = repository;
@@ -42,15 +48,31 @@ public class FlightService {
         return result;
     }
 
-    private FlightDto convertToFlightDto(AirData airData) {
+    public FlightDto convertToFlightDto(AirData airData) {
+        if (airData == null) {
+            throw new IllegalArgumentException("AirData is null");
+        }
         FlightDto flightDto = new FlightDto();
-        flightDto.setFlightNumber(airData.getFlight().getNumber());
-        // flightDto.setAircraftRegistration(airData.getAircraft().getRegistration());
-        flightDto.setAirlineName(airData.getAirline().getName());
-        flightDto.setDepartureAirport(airData.getDeparture().getAirport());
-        flightDto.setArrivalAirport(airData.getArrival().getAirport());
-        flightDto.setDepartureTime(airData.getDeparture().getEstimated());
-        flightDto.setArrivalTime(airData.getArrival().getEstimated());
+        flightDto.setFlightNumber(Optional.of(airData)
+                .map(AirData::getFlight)
+                .map(FlightIdentifier::getNumber)
+                .orElse("-"));
+
+        Optional<TerminalPoint> departure = Optional.of(airData).map(AirData::getDeparture);
+        flightDto.setDepartureAirport(departure.map(TerminalPoint::getIata).orElse(DEFAULT_VALUE));
+        flightDto.setDepartureTime(departure.map(TerminalPoint::getScheduled).orElse(DEFAULT_VALUE));
+        Optional<TerminalPoint> arrival = Optional.of(airData).map(AirData::getArrival);
+        flightDto.setArrivalAirport(arrival.map(TerminalPoint::getIata).orElse(DEFAULT_VALUE));
+        flightDto.setArrivalTime(arrival.map(TerminalPoint::getScheduled).orElse(DEFAULT_VALUE));
+        flightDto.setAirlineName(Optional.of(airData)
+                .map(AirData::getAirline)
+                .map(Airline::getName)
+                .orElse(DEFAULT_VALUE));
+        flightDto.setAircraftRegistration(Optional.of(airData)
+                .map(AirData::getAircraft)
+                .map(Aircraft::getRegistration)
+                .orElse(DEFAULT_VALUE));
+
         return flightDto;
     }
 }
---
+++ src/test/java/al/sda/flyspy/domain/flight/service/FlightServiceTest.java
@@ -0,0 +1,95 @@
+package al.sda.flyspy.domain.flight.service;
+
+import al.sda.flyspy.domain.flight.model.dto.FlightDto;
+import al.sda.flyspy.domain.flight.model.dto.airdata.AirData;
+import al.sda.flyspy.domain.flight.model.dto.airdata.Aircraft;
+import al.sda.flyspy.domain.flight.model.dto.airdata.Airline;
+import al.sda.flyspy.domain.flight.model.dto.airdata.FlightIdentifier;
+import al.sda.flyspy.domain.flight.model.dto.airdata.TerminalPoint;
+import al.sda.flyspy.domain.flight.model.entity.Flight;
+import al.sda.flyspy.shared.Repository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FlightServiceTest {
+    @Mock
+    private AirDataService airDataService;
+    @Mock
+    private Repository<Flight, Long> repository;
+    private FlightService flightService;
+
+    @BeforeEach
+    void setUp() {
+        repository = Mockito.mock(Repository.class);
+        airDataService = Mockito.mock(AirDataService.class);
+        flightService = new FlightService(repository, airDataService);
+    }
+
+    @Test
+    void testParseAirData() {
+
+        //Arrange
+        AirData airData = Mockito.mock(AirData.class);
+        FlightIdentifier flightIdentifier = Mockito.mock(FlightIdentifier.class);
+        Aircraft aircraft = Mockito.mock(Aircraft.class);
+        Airline airline = Mockito.mock(Airline.class);
+        TerminalPoint departure = Mockito.mock(TerminalPoint.class);
+        TerminalPoint arrival = Mockito.mock(TerminalPoint.class);
+        Mockito.when(airData.getAircraft()).thenReturn(aircraft);
+        Mockito.when(airData.getFlight()).thenReturn(flightIdentifier);
+        Mockito.when(airData.getDeparture()).thenReturn(departure);
+        Mockito.when(airData.getArrival()).thenReturn(arrival);
+        Mockito.when(airData.getAirline()).thenReturn(airline);
+
+        Mockito.when(airData.getFlight().getNumber())
+                .thenReturn("LH175");
+        Mockito.when(airData.getDeparture().getIata())
+                .thenReturn("TIA");
+        Mockito.when(airData.getDeparture().getScheduled())
+                        .thenReturn("2025-05-18T00:20:00+00:00");
+        Mockito.when(airData.getArrival().getIata())
+                .thenReturn("FRA");
+        Mockito.when(airData.getArrival().getScheduled())
+                .thenReturn("2025-05-20T00:20:00+00:00");
+        Mockito.when(airData.getAirline().getName())
+                .thenReturn("Lufthansa");
+        Mockito.when(airData.getAircraft().getRegistration())
+                .thenReturn("Boeing");
+
+        //Act
+        FlightDto actual = flightService.convertToFlightDto(airData);
+
+        //Assertion
+        FlightDto expected = new FlightDto();
+        expected.setAircraftRegistration("Boeing");
+        expected.setAirlineName("Lufthansa");
+        expected.setFlightNumber("LH175");
+        expected.setArrivalAirport("FRA");
+        expected.setDepartureAirport("TIA");
+        expected.setDepartureTime("2025-05-18T00:20:00+00:00");
+        expected.setArrivalTime("2025-05-20T00:20:00+00:00");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testParseAirline_InvalidFlightNumber() {
+        AirData airData = new AirData();
+
+        FlightDto actual = flightService.convertToFlightDto(airData);
+        FlightDto expected = new FlightDto();
+        expected.setFlightNumber("-");
+        expected.setDepartureAirport("-");
+        expected.setDepartureTime("-");
+        expected.setArrivalAirport("-");
+        expected.setArrivalTime("-");
+        expected.setAircraftRegistration("-");
+        expected.setAirlineName("-");
+
+        assertEquals(expected, actual);
+    }
+}
